5eecb61c6a3eb2290aefc92ebd36cd531f025034
mihajlovice973 <mihajlovice973@gmail.com>
2026-07-16 15:24:00 +0500

    Create shop_items.lua

---
+++ shop_items.lua
@@ -0,0 +1,23 @@
+local items = {}
+items.sellItems = {
+    {displayName = "Деньги",                  internalName = "customnpcs:npcMoney",       qty = 1308586, price = 1.00},
+    {displayName = "Малахит",                 internalName = "dwcity:Malahit", qty = 1310720, price = 500},
+    {displayName = "Упакованный трофей",      internalName = "dwcity:TrophyBox",  qty = 1310718, price = 150},
+    {displayName = "Камень воскрешения",      internalName = "DraconicEvolution:draconium",     qty = 1294724, price = 978.00, damage = 1 },
+    {displayName = "Солнечная пыль",          internalName = "dwcity:Splitter_dust",  qty = 1310718, price = 75},
+    {displayName = "Железная руда",           internalName = "minecraft:iron_ore",     qty = 1294724, price = 0.25},
+    {displayName = "Железный блок",           internalName = "minecraft:iron_block",     qty = 1309081, price = 0.95},
+    {displayName = "Железный слиток",         internalName = "minecraft:iron_ingot",     qty = 1294724, price = 0.11},
+    {displayName = "Медный слиток",           internalName = "IC2:itemIngot",      qty = 1310720, price = 0.25},
+    {displayName = "Медная руда",             internalName = "IC2:blockOreCopper",     qty = 1310709, price = 0.95},
+    {displayName = "Палладий-106",            internalName = "dwcity:X32",     qty = 1310709, price = 157.9}
+
+}
+items.vanillaItems = {
+    "IC2:itemIngot",
+    "IC2:blockMetal",
+    "minecraft:iron_block",
+    "minecraft:iron_ingot",
+    -- ...
+}
+return items
